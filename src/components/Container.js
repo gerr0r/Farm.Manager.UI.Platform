a1872d39f5875f5d98cc04603d60a92ae2c61cba
Gergan Ruschev <gergan.ruschev@yara.com>
2021-07-06 14:12:55 +0300

    feat/farm admin routes added

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,26 +1,60 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-const InactiveAccounts = React.lazy(() => import('plugins/InactiveAccounts'))
-const ActiveAccounts = React.lazy(() => import('plugins/ActiveAccounts'))
-const Countries = React.lazy(() => import('plugins/Countries'))
-const Account = React.lazy(() => import('plugins/Account'))
+let InactiveAccounts, ActiveAccounts, Account, Countries, Country
+let Farms, Farm, Users, User, Employees, Employee, Profile
 
 const Container = ({ role }) => {
+    switch (role) {
+        case "master":
+            InactiveAccounts = React.lazy(() => import('plugins/InactiveAccounts'))
+            ActiveAccounts = React.lazy(() => import('plugins/ActiveAccounts'))
+            Account = React.lazy(() => import('plugins/Account'))
+            Countries = React.lazy(() => import('plugins/Countries'))
+            Country = React.lazy(() => import('plugins/Country'))
 
-    return (
-        <div className="container">
-            <React.Suspense fallback={<span>Loading...</span>}>
-                <Switch>
-                    <Route path='/inactive-accounts/:accountId'><Account /></Route>
-                    <Route path='/inactive-accounts'><InactiveAccounts /></Route>
-                    <Route path='/active-accounts/:accountId'><Account /></Route>
-                    <Route path='/active-accounts'><ActiveAccounts /></Route>
-                    <Route path='/countries'><Countries /></Route>
-                </Switch>
-            </React.Suspense>
-        </div>
-    )
+            return (
+                <div className="container">
+                    <React.Suspense fallback={<span>Loading...</span>}>
+                        <Switch>
+                            <Route path='/inactive-accounts/:accountId'><Account /></Route>
+                            <Route path='/inactive-accounts'><InactiveAccounts /></Route>
+                            <Route path='/active-accounts/:accountId'><Account /></Route>
+                            <Route path='/active-accounts'><ActiveAccounts /></Route>
+                            <Route path='/countries/:countryCode'><Country /></Route>
+                            <Route path='/countries'><Countries /></Route>
+                        </Switch>
+                    </React.Suspense>
+                </div>
+            )
+        case "admin":
+            Farms = React.lazy(() => import('plugins/Farms'))
+            Farm = React.lazy(() => import('plugins/Farm'))
+            Users = React.lazy(() => import('plugins/Users'))
+            User = React.lazy(() => import('plugins/User'))
+            Employees = React.lazy(() => import('plugins/Employees'))
+            Employee = React.lazy(() => import('plugins/Employee'))
+            Profile = React.lazy(() => import('plugins/Profile'))
+
+            return (
+                <div className="container">
+                    <React.Suspense fallback={<span>Loading...</span>}>
+                        <Switch>
+                            <Route path='/farms/:farmId'><Farm /></Route>
+                            <Route path='/farms'><Farms /></Route>
+                            <Route path='/users/:userId'><User /></Route>
+                            <Route path='/users'><Users /></Route>
+                            <Route path='/employees/:employeeId'><Employee /></Route>
+                            <Route path='/employees'><Employees /></Route>
+                            <Route path='/profile'><Profile /></Route>
+                        </Switch>
+                    </React.Suspense>
+                </div>
+            )
+    
+        default:
+            return null;
+    }
 }
 
 export default Container
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from "react";
 
-import Header from '../components/Header'
-import SideMenu from '../components/SideMenu'
-import Container from '../components/Container'
-import Footer from '../components/Footer'
+import Header from "../components/Header";
+import SideMenu from "../components/SideMenu";
+import Container from "../components/Container";
+import Footer from "../components/Footer";
 
-import { useAuthContext } from '../AuthContext'
+import { useAuthContext } from "../AuthContext";
 
 const Main = () => {
-    const { account } = useAuthContext()
+  const { account } = useAuthContext();
 
-    return (
-        <div className="main">
-            <Header />
-            <div className="content">
-                <SideMenu role={account.role} />
-                <Container role={account.role} />
-            </div>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div className="main">
+      <Header />
+      <div className="content">
+        <SideMenu role={account.role} />
+        <Container role={account.role} />
+      </div>
+      <Footer />
+    </div>
+  );
+};
 
-export default Main
+export default Main;
